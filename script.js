798d3d1cf21ddfd8dfe88b40ccd271bae95268ae
Antor Debnath <antordebnath686@gmail.com>
2025-05-04 06:43:28 +0600

    personal dashboard

---
+++ script.js
@@ -0,0 +1,50 @@
+function toggleTheme(){
+    document.body.classList.toggle("dark")
+}
+
+function updateClock(){
+    const timeEL = document.getElementById("time");
+const now = new Date()
+timeEL.textContent = now.toLocaleTimeString()
+}
+
+setInterval(updateClock, 1000);
+
+fetch("http://api.weatherapi.com/v1/current.json?key=537e34532be34dc6bab164914251004&q=Bangladesh").then((resp) => resp.json()).then((data) => {document.getElementById("weatherInfo").textContent = `${data.location.name} ${data.current.temp_c} deg C, ${data.current.condition.text}`}).catch(err => {
+    document.getElementById("weatherInfo").textContent = "give proper name"
+});
+
+
+function addtodo(){
+    const input = document.getElementById("todoinput");
+    const list = document.getElementById("todolist");
+    const li =document.createElement("li");
+    li.textContent =input.value;
+    li.addEventListener("click", ()=> li.remove())
+    list.appendChild(li);
+    input.value = ""
+}
+
+function savenote(){
+    const note = document.getElementById("notearea").value
+    localStorage.setItem("note",note);
+    displaynote();
+}
+
+function displaynote(){
+    const note = localStorage.getItem("note");
+    document.getElementById("shownote").textContent = note;
+}
+displaynote();
+
+document.getElementById("contactform").addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const formdata = new FormData(event.target);
+    const entries = [...formdata.entries()].reduce((acc,[k,v])=>{
+        acc[k] = v;
+        return acc;
+    }, {})
+    
+    console.log(entries)
+    event.target.reset()
+})
